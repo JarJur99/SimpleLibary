0b326a8897a36b599384b91892ebf0ca93cdad4d
JarJur99 <84501697+JarJur99@users.noreply.github.com>
2021-05-20 12:33:45 +0200

    Dodano projekt prostej biblioteki

---
+++ StronaKsiazek/StronaKsiazek/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StronaKsiazek.Model;
+
+namespace StronaKsiazek.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public BookController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+        //[HttpDelete]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var bookFromDb = await _db.Book.FirstOrDefaultAsync(book => book.Id == id);
+
+        //}
+    }
+}
